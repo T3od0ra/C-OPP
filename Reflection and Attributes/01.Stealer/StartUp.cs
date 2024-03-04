3b6c9a6bd0bafdb15e204170fdfe0a56d69c3a4b
Teodora Petkova <125208300+T3od0ra@users.noreply.github.com>
2024-03-04 17:00:05 +0200

    Add files via upload

---
+++ Reflection and Attributes/01.Stealer/StartUp.cs
@@ -0,0 +1,13 @@
+﻿namespace Stealer
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            Spy spy = new Spy();
+            string result = spy.StealFieldInfo("Stealer.Hacker", "username", "password");
+            Console.WriteLine(result);
+        }
+    }
+    
+}
