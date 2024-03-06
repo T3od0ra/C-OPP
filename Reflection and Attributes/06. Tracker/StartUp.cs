ccc288a1c0b24815f4f17d38bb9d9f45a9b8dc60
Teodora Petkova <125208300+T3od0ra@users.noreply.github.com>
2024-03-06 17:59:11 +0200

    Add files via upload

---
+++ Reflection and Attributes/06. Tracker/StartUp.cs
@@ -0,0 +1,25 @@
+﻿namespace AuthorProblem
+{
+    [Author("Victor")]
+     public class StartUp
+    {
+        [Author("George")]
+        static void Main(string[] args)
+        {
+            var tracker = new Tracker();
+            tracker.PrintMethodsByAuthor();
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true)]
+    class AuthorAttribute : Attribute
+    {
+        public AuthorAttribute(string name)
+        {
+            this.Name = name;
+        }
+        public string Name { get; set; }
+    }
+}
+
+
