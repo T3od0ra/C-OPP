f040b2fd686c161ace8332f1fa0009f102a3d090
Teodora Petkova <125208300+T3od0ra@users.noreply.github.com>
2024-03-06 17:15:36 +0200

    Add files via upload

---
+++ Reflection and Attributes/03. Mission Private Impossible/03. Mission Private Impossible/StartUp.cs
@@ -0,0 +1,15 @@
+﻿using _Stealer;
+
+namespace Stealer
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            Spy spy = new Spy();
+            string result = spy.RevealPrivateMethods("Stealer.Hacker");
+            Console.WriteLine(result);
+        }
+    }
+   
+}
